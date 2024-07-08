fa7980a46d6c1f7f56380609bb4b7ea44d5036fa
surya <ssuryakumar97@gmail.com>
2024-07-08 11:40:53 +0530

    updated connectjs

---
+++ db/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const db = async() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+        console.log("DB connection established")
+    }catch(err){
+        console.log("DB Error", err);
+    }
+}
+
+module.exports = db;
